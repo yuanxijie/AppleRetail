a4eb46346b58fc5d17a1351942d909a7ab52908c
yuanxijie <yxj_zju@163.com>
2011-12-27 23:40:41 +0800

    add csdn

--- CsdnLogin.py
+++ CsdnLogin.py
@@ -1,2 +1,19 @@
 __author__ = 'yuan'
 
+import urllib
+import urllib2
+import cookielib
+
+url = "http://passport.csdn.net/account/login"
+
+class Csdn:
+    cookie_file = "./cookie.txt"
+    def __init__(self, user, passwd):
+        self.username = user
+        self.passwd = passwd
+    def __login(self):
+        ckjar = cookielib.MozillaCookieJar(Csdn.cookie_file)
+        ckproc = urllib2.HTTPCookieProcessor(ckjar)
+        opener = urllib2.build_opener(ckproc)
+
+
