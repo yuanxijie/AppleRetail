8785571dff5c86133025a18c268bbf4b3e307510
yuanxijie <yxj_zju@163.com>
2011-12-29 23:25:18 +0800

    enable cookie

--- CsdnLogin.py
+++ CsdnLogin.py
@@ -5,15 +5,33 @@ import urllib2
 import cookielib
 
 url = "http://passport.csdn.net/account/login"
-
+#url = "http://www.baidu.com"
 class Csdn:
     cookie_file = "./cookie.txt"
     def __init__(self, user, passwd):
         self.username = user
         self.passwd = passwd
-    def __login(self):
+    def login(self):
         ckjar = cookielib.MozillaCookieJar(Csdn.cookie_file)
-        ckproc = urllib2.HTTPCookieProcessor(ckjar)
-        opener = urllib2.build_opener(ckproc)
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(ckjar))
+        req = urllib2.Request(url)
+        req.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:9.0.1) Gecko/20100101 Firefox/9.0.1')
+        res = opener.open(req)
+        html = res.read()
+        res.close()
 
+        print html
+        ckjar.save(filename=self.cookie_file, ignore_discard=False, ignore_expires=False)
+
+        req = urllib2.Request("http://counter.csdn.net/pv.aspx?id=20")
+        req.add_header('Referer', url)
+        res = opener.open(req)
+        print res.read();
+        res.close()
+        ckjar.save(filename=self.cookie_file, ignore_discard=True, ignore_expires=True)
+
+
+if __name__ == '__main__':
+    csdn = Csdn('hotforever', 'baidu_c0m')
+    csdn.login()
 
